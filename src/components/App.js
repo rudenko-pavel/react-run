14143a2ff40bff1638db57b3177d49266f096c74
FASTANDSMART\admin <pavel.v.rudenko@gmail.com>
2020-01-14 14:14:47 +0200

    create react side
    1. console:
    	npx create-react-app <name of project>
    	npm i typescript --save
    	npm i node-sass --save
    	npm i sass --save
    	npm i axios@0.18.1
    	npm i semantic-ui-css --save
    	npm i --save redux react-redux
    	npm i --save redux-thunk
    
        npm start

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scss/App.scss';
+
+const App = () =>{
+    return (
+        <div className="ui container">
+            App
+        </div>
+    )
+};
+
+export default App;
